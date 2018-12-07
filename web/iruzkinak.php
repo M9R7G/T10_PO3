c0cbabad9fc3076720e878bc7129dfb781c83c91
iperezz <45682937+iperezz@users.noreply.github.com>
2018-12-07 13:22:22 +0100

    Add files via upload
    Borratu ditut behar ez genituen fitxategiak eta zuzendu ditut oraingoz egin ditugunak

---
+++ web/iruzkinak.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>IRUZKINAK</title>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<link rel="stylesheet" type="text/css" href="style.css">
+        <div>
+	    	HEMEN PELIKULA BAKOITZEKO IRUZKINAK IKUS DAITEZKE; nork, noiz eta zer idatzi duen. <br>
+	    	<a href="login0.php">LOGIN</a> <br>
+	    	<a href="register.php">REGISTER</a><br>
+	    	<a href="index.html">ATZERA</a><br>
+	    </div>
+		<?php
+			if(!file_exists($BL_FILE))
+			{
+				echo('<p>erabiltzaile liburua hutsik dago. Iruzkin bat idazten lehenengoa izan nahi baduzu klikatu <a href="iruzkina_gehitu.html">hemen</a>.</p>');
+			}
+			elseif(!($bl=simplexml_load_file($BL_FILE)))
+			{
+				echo('<p>Errore bat gertatu da erabiltzaile liburua irakurtzean. Barkatu eragozpenak</p>');
+			}
+			else
+			{
+			foreach($bl->erabiltzaile as $erabiltzaile)
+				{
+						echo('<div class="iruzkina">');
+						echo('<div class="ir_goiburua">');
+						echo('<span class="izena">'.$erabiltzaile->izena.'</span>');
+						echo('<span class="data">'.$erabiltzaile->data.'</span>');
+						echo('</div>');
+						echo('<div class="ir_gorputza" id="'.$erabiltzaile['id'].'">');
+						echo('</div>');
+				}
+		?>
+	</body>
+
+</html>
---
+++ web/pasahitza.php
@@ -0,0 +1,66 @@
+<?php
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+		$name = $_POST["erabiltzailea"];
+		$email = $_POST["emaila"];
+		if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+			echo "<script>alert('Emaila oker idatzi duzu')</script>";
+		}
+		else if(!preg_match('/^[a-zA-Z].*[\s\.]*$/',$name)){
+			echo "<script>alert('Izen bat idatzi behar duzu')</script>";
+		}
+		else{
+			$erabk = simplexml_load_file("data/erabiltzaileak.xml");
+			foreach($erabk->erabiltzaile as $erab){
+				if($erab->izena==$name && $erab->emaila==$email){
+					echo "<script>alert('Zure pasahitza hau da: ".$erab->pasahitza."');window.location.href = 'login0.php'</script>";
+				}
+			}
+			echo "<script>alert('Erabiltzailea ez da existitzen')</script>";
+		}
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Pasahitza berreskuratu</title>
+		<meta charset="UTF-8">
+
+		<script type="text/javascript" src="funtzioak.js"></script>
+	</head>
+	<body>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<div align="right">
+			<a href="login0.html">LOGIN</a><br>
+			<a href="register.html">ERREGISTRATU</a><br>
+			<a href="pelikulak.html">IRUZKINAK IKUSI</a><br>
+			<a href="index.html">ATZERA</a>
+		</div>
+
+		<h2>Berreskuratu zure pasahitza</h2>
+		<form name="f" method="post" action="pasahitza.php">
+			<center>
+			 <div class="container">
+
+			 	<b>Erabiltzaile izena: </b>
+			 	<input type="text" placeholder="Sartu izena" name="erabiltzailea">
+			 	<br>
+			 	
+    			<b>Zure e-mail kontua: </b>
+    			<input type="text" placeholder="adibide@bat.da"  name="emaila" >
+    			<br>
+				<!--
+    			<b>Pasahitza: </b>
+    			<input type="password" placeholder="Sartu pasahitza" name="pasahitza" >
+				-->
+    		</div>
+    		<h3>Eremu guztiak betetzea derrigorrezkoa da.</h3>
+    	   	</br>
+			<input type="submit" value="PASAHITZA BERRESKURATU">                 
+
+			<button type="reset" class="reset">Ezeztatu</button>
+			<br><br>
+	    		</center>
+		</form>
+	</body>
+</html>
